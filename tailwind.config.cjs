8edb50f20ab9db547a59d61f8b6ceaddfdcfa65f
Leonardo Alipazaga <leonardoalipazagab@gmail.com>
2023-06-22 17:00:35 -0500

    feat: add slide in and slide out to display navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,37 @@ module.exports = {
     fontFamily: {
       sans: ['Gotham Book, Gotham, sans-serif'],
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-out': {
+          '0%': {
+            transform: `translateX(0)`,
+          },
+          '20%': {
+            transform: `translateX(100px)`,
+            opacity: 0.1,
+          },
+          '100%': {
+            transform: `translateX(280px)`,
+            opacity: 0,
+          },
+        },
+        'slide-in': {
+          from: {
+            transform: `translateX(280px)`,
+            opacity: 0,
+          },
+          to: {
+            transform: `translateX(0)`,
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        'slide-out': 'slide-out 1s ease-out forwards',
+        'slide-in': 'slide-in 0.7s ease-in-out forwards',
+      },
+    },
   },
   plugins: [],
 };
